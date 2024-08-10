d2860ee287c2678a1f85b952384420384612f236
sotasage <sotasota.0702@gmail.com>
2024-08-11 08:55:05 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">麻雀順位点計算ソフト</h1>
+    </main>);
+}
